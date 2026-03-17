734e62472845f9a45beb398129b52388c55e7378
Isming <email@example.com>
2026-03-17 08:45:04 +0500

    qoshildi

---
+++ main.js
@@ -0,0 +1,20 @@
+let userEmail = document.getElementById("userEmail");
+let btn = document.getElementById("btn");
+let userName = document.getElementById("userName")
+const text =document.getElementById("text");
+
+
+const useIsm =("farhod")
+const valiEmail = ("user123gmail.com")
+
+btn.addEventListener("click", function(e){
+    e.preventDefault()
+  if (userEmail.value === useIsm && email.value === valiEmail) {
+    this.textContent = "royxatdan otinggiz"
+
+    
+  } else {
+    this.textContent = "email yoki usrnameni tog'ri kirit "
+    
+  }
+})
