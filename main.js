3835674cda6885ff43abe4951485f5d750b27139
Isming <email@example.com>
2026-03-17 13:06:35 +0500

    hdhdehd

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 let userEmail = document.getElementById("userEmail");
 let btn = document.getElementById("btn");
 let userName = document.getElementById("userName")
-const text =document.getElementById("text");
+const text = document.getElementById("text");
 
 
-const useIsm =("farhod")
-const valiEmail = ("user123gmail.com")
+const useIsm ="farhod"
+const valiEmail = "user123gmail.com"
 
-btn.addEventListener("click", function(e){
+btn.addEventListener("click",function(e){
     e.preventDefault()
-  if (userEmail.value === useIsm && email.value === valiEmail) {
-    this.textContent = "royxatdan otinggiz"
+  if (userEmail.value === useIsm && useIsm.value === valiEmail) {
+    text.textContent = "royxatdan otinggiz"
 
     
   } else {
-    this.textContent = "email yoki usrnameni tog'ri kirit"
+    text.textContent = "email yoki usrnameni tog'ri kirit"
     
   }
 
